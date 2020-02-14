935d9f4d70d1884f1f50599e3e9d364a12393e53
bibeksoft <bibek2you@hotmail.com>
2020-02-14 18:31:14 +0530

    Done with Data binding
    Signed-off-by: bibeksoft <bibek2you@hotmail.com>

--- src/app/Employee/project-delete/project-delete.component.ts
+++ src/app/Employee/project-delete/project-delete.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
+import {ProjectData} from '../project-details/projectDataDetails'
 @Component({
   selector: 'app-project-delete',
   templateUrl: './project-delete.component.html',
   styleUrls: ['./project-delete.component.css']
 })
 export class ProjectDeleteComponent implements OnInit {
-
+  deleteDetails1=ProjectData;;
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
