4ea33a6fa6fa2ea05b427a1fbd2a13cd2f14e685
bibeksoft <bibek2you@hotmail.com>
2020-02-27 18:48:14 +0530

    firbase testing
    Signed-off-by: bibeksoft <bibek2you@hotmail.com>

--- src/app/Employee/program-team/program-team.component.ts
+++ src/app/Employee/program-team/program-team.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit,Input } from '@angular/core';
 import { EmployeeDetails } from '../employee-details/EmployeeDetails';
+import {AngularFirestore} from '@angular/fire/firestore'
+
 @Component({
   selector: 'app-program-team',
   templateUrl: './program-team.component.html',
@@ -7,9 +9,21 @@ import { EmployeeDetails } from '../employee-details/EmployeeDetails';
 })
 export class ProgramTeamComponent implements OnInit {
  @Input() selectProg:EmployeeDetails;
-  constructor() { }
+details :any [];
+  constructor(private fire:AngularFirestore) { 
+    
+    
+   }
 
   ngOnInit() {
+    this.fire.collection('Employee').snapshotChanges().subscribe(Edetails=>
+      {
+        this.details=Edetails.map(X=>{
+          return  X.payload.doc.data();
+      })
+       
+      })
   }
+  
 
 }
